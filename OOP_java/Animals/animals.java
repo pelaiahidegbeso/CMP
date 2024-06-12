e94bc8165c9e9c8b837b89471c655f2c1244ecc7
pelaiahidegbeso <132790365+pelaiahidegbeso@users.noreply.github.com>
2024-06-12 16:54:18 +0100

    Create animals.java

---
+++ OOP_java/Animals/animals.java
@@ -0,0 +1,17 @@
+
+package OOP_java.Animals;
+
+public class Animal {
+    String name;
+
+    String move() {
+        return "moves";
+    }
+    String sound() {
+        return "makes sound";
+    }
+
+    boolean isAlive() {
+        return true;
+    }
+}
